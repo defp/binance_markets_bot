9b9044f38bfce826bd98608eb8c9629919a88738
ifsc01 <ifsclimbing@gmail.com>
2018-05-09 14:15:50 +0800

    config nadia

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule BinanceMarketsBot.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :websockex],
+      extra_applications: [:logger, :websockex, :nadia],
       mod: {BinanceMarketsBot.Application, []}
     ]
   end
