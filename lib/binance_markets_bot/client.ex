5a9630a2aace7a0590be4d5ba69fd6a51f846aa3
ifsc01 <ifsclimbing@gmail.com>
2018-05-09 15:25:03 +0800

    handle func

--- lib/binance_markets_bot/client.ex
+++ lib/binance_markets_bot/client.ex
@@ -1,7 +1,25 @@
 defmodule BinanceMarketsBot.Client do
   use WebSockex
+  require Logger
 
   def start_link(state) do
-    WebSockex.start_link("wss://stream.binance.com:9443/ws/!ticker@arr", __MODULE__, state, debug: [:trace])
+    options = if Mix.env() == :dev, do: [debug: [:trace]], else: []
+    ws_endpoint = "wss://stream.binance.com:9443/ws/!ticker@arr"
+    WebSockex.start_link(ws_endpoint, __MODULE__, state, options)
+  end
+
+  def handle_frame({type, msg}, state) do
+    Logger.info("Received Message - Type: #{inspect(type)} -- Message: #{msg}")
+    {:ok, state}
+  end
+
+  def handle_disconnect(%{reason: reason}, state) do
+    Logger.info("Local close with reason: #{inspect(reason)}")
+    {:ok, state}
+  end
+
+  def terminate(reason, _state) do
+    Logger.info("Local terminate with reason: #{inspect(reason)}")
+    exit(:normal)
   end
 end
